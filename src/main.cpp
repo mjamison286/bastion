55171c32f04539d1b5653549aaa18044d0149af5
mjamison286 <mjamison286@gmail.com>
2026-08-30 03:32:55 -0500

    created the cmakelists file and the main fil, none of which have any content

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+//header files here
+
+//argc is the amount of args passed into the program, while the argv array are the actual arguments as c-strings
+int main(int argc, char* argv[])
+{
+    //actual logic
+
+    return 0;
+}
